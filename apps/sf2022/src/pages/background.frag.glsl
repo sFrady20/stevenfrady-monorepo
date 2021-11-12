df66e61e1e3ec7015e6b864141c4bd5f4fd1e058
zabuter <sfrady20@gmail.com>
2021-11-11 23:50:23 -0500

    multi bubble disabled

--- apps/sf2022/src/pages/background.frag.glsl
+++ apps/sf2022/src/pages/background.frag.glsl
@@ -6,12 +6,15 @@ uniform vec2 cursorSpring;
 uniform vec2 scroll;
 uniform float transition;
 
-vec3 bubbleBaseCol = vec3(0.1,0.4,.2);
-vec3 bubbleHighlightCol = vec3(5., 2.5, 3.);
+const vec3 bubbleBaseCol = vec3(0.1,0.4,.2);
+const vec3 bubbleHighlightCol = vec3(5., 2.5, 3.);
+
+vec2 aspect;
 
 float rand(vec2 n) { 
 	return fract(sin(dot(n, vec2(132.9898, 34.1414))) * (43758.5453 + floor(transition + 0.5) + seed * 0.74235));
 }
+
 float noise(vec2 p){
 	vec2 ip = floor(p);
 	vec2 u = fract(p);
@@ -25,12 +28,32 @@ float noise(vec2 p){
 }
 
 //bubbles
-mat2 m(float a){float c=cos(a), s=sin(a);return mat2(c,-s,s,c);}
+mat2 m(float a){
+	float c=cos(a), s=sin(a);
+	return mat2(c,-s,s,c);
+}
 
-float map(vec3 p){
-    p.xz*= m(time*0.4);p.xy*= m(time*0.3);
-    vec3 q = p*1.+time;
-    return length(p+vec3(sin(time*0.7)))*log(length(p)+1.) + sin(q.x+sin(q.z+sin(q.y)))*0.5 - 1.;
+float etherMap(vec3 p, float t){
+	p.xz*= m(t*0.4);p.xy*= m(t*0.3);
+	vec3 q = p*1.+t;
+	return length(p+vec3(sin(t*0.7)))*log(length(p)+1.) + sin(q.x+sin(q.z+sin(q.y)))*0.5 - 1.;
+}
+
+vec3 etherBubble (vec2 p, float t, float s) {
+	vec3 cl = vec3(0.);
+	float d = 2.5;
+
+	float ts = 2. + (1.-s) * 20.;
+
+	for(int i=0; i<=5; i++)	{
+		vec3 p = vec3(0,0,5.) + normalize(vec3(p * ts, -1.))*d;
+    float rz = etherMap(p, t);
+		float f = clamp((rz - etherMap(p+.1, t))*0.5, -.1, 1. );
+    vec3 l = bubbleBaseCol + bubbleHighlightCol*f;
+    cl = cl*l + smoothstep(2.5, .0, rz)*.7*l;
+		d += min(rz, 1.);
+	}
+  return vec3(cl);
 }
 
 vec3 etherLayer(vec2 p) {
@@ -45,35 +68,42 @@ vec3 etherLayer(vec2 p) {
 	//transform
 	p -= vec2(0.5, 0.5);
 	//scale
-	p *= 2.;
+	p *= aspect;
 
-	//rectify
-	p.y *= resolution.y/resolution.x;
+	vec3 rCol = vec3(0.);
 
-	vec3 cl = vec3(0.);
-	float d = 2.5;
-	for(int i=0; i<=5; i++)	{
-		vec3 p = vec3(0,0,5.) + normalize(vec3(p, -1.))*d;
-    float rz = map(p);
-		float f = clamp((rz - map(p+.1))*0.5, -.1, 1. );
-    vec3 l = bubbleBaseCol + bubbleHighlightCol*f;
-    cl = cl*l + smoothstep(2.5, .0, rz)*.7*l;
-		d += min(rz, 1.);
-	}
-  return vec3(cl);
+	rCol += etherBubble(p, time, 1.);
+
+	// float rep = p.y;
+	// for(int i=0; i<1; i++)	{
+	// 	float rpx = (noise(vec2(float(i) * 62.415, seed)) * 1. - 0.5) * aspect.x;
+	// 	float rpy = (noise(vec2(float(i) * 53.52, seed)) * 1. - 0.5) * aspect.y;
+
+	// 	float rpt = time + noise(vec2(float(i) * 125.35, seed)) * 100.;
+
+	// 	float rps = noise(vec2(seed, float(i) * 125.35)) * 0.8;
+
+	// 	rCol += etherBubble(p + vec2(rpx, rpy), rpt, rps);
+	// }
+
+	return rCol;
 }
 
 void main() {
+	aspect = vec2(
+		min(resolution.x/resolution.y, 1.),
+		min(resolution.y/resolution.x, 1.)
+	);
+
   vec2 p = gl_FragCoord.xy;
-  vec2 uv = gl_FragCoord.xy / resolution.xy;
-	vec2 xuv = gl_FragCoord.xy / max(resolution.x,resolution.y);
+  vec2 uv = gl_FragCoord.xy / resolution.xy * aspect;
 
 	vec3 col = vec3(0.);
 	float a = 0.;
 
 	//bg
-  float m = noise(xuv*3.);
-	col = mix(col, vec3((xuv.x)*.4,xuv.x,xuv.y), m);
+  float m = noise(uv*3.);
+	col = mix(col, vec3((uv.x)*.4,uv.x,uv.y), m);
 	a += m;
 
 	//ether
